a6439a3d3bdea4cc28e3c61b58011f6fef1ea79a
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-06-21 21:41:12 -0300

    Incluindo firebase no projeto

--- web/src/components/banner/banner.component.jsx
+++ web/src/components/banner/banner.component.jsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Profile from '../lopesProfile/profile.component';
-import gabrielImg from '../../images/Gabriel.png';
-import matheusImg from '../../images/matheus.png';
+import firebase from '../../firebase';
 import './banner.style.scss';
 
 const Banner = () => {
-    return(
-        <Container className="bannerContainer" fluid>
-            <Row>
-                <Col md={6} sm={12}>
-                    <Profile nome="Matheus de Oliveira Lopes" imagem={matheusImg} skills={["React", "Spring", "SQL"]} />
-                </Col>
-                <Col md={6} sm={12}>
-                    <Profile nome="Gabriel de Oliveira Lopes" imagem={gabrielImg} skills={["Spring", "Angular", "React", "Banco Relacional"]} />
-                </Col>
-            </Row>
-        </Container>
-    )
+
+    const [dados, setDados] = React.useState();
+    const [initializing, setInitializing] = React.useState(true);
+
+    React.useEffect(() => {
+        firebase.firestore().collection("dadosDinamicos").doc('paginaDevLopes')
+            .get().then(doc => {
+                if(doc.exists) {
+                    const { banner } = doc.data();
+
+                    setDados(banner);
+                    setInitializing(false);
+                } else {
+                    console.error("Documento não foi encontrado!");
+                }
+            });
+    }, [setDados, setInitializing]);
+
+    if(initializing)
+        return (
+            <Container className="bannerContainer" fluid>
+                <Row>
+                    <Col sm={12} style={{flex: 1, flexDirection:'column', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <FontAwesomeIcon icon={faSpinner} size="5x" spin />
+                        <h3>Buscando dados...</h3>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    else
+        return(
+            <Container className="bannerContainer" fluid>
+                <Row>
+                    {
+                        dados.map(item => {
+                            return(
+                                <Col md={6} sm={12}>
+                                    <Profile nome={item.title} imagem={item.image} skills={item.skills} />
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
+        )
 }
 
 export default Banner;
